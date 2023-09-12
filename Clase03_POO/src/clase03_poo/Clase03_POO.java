38bf7d5aff8746125d0609c7590f4e9cb75d6c3c
IsaelFatamaDev <isael.fatama@vallegrande.edu.pe>
2023-09-12 09:40:27 -0500

    Resubido

--- Clase03_POO/src/clase03_poo/Clase03_POO.java
+++ Clase03_POO/src/clase03_poo/Clase03_POO.java
@@ -5,20 +5,23 @@ public class Clase03_POO {
     public static void main(String[] args) {
         Persona persona1 = new Persona();
         persona1.nombre = "Juan";
-        persona1.edad = 15;
+        persona1.edad = 30;
 
         Persona persona2 = new Persona();
         persona2.nombre = "María";
-        persona2.edad = 30;
+        persona2.edad = 12;
 
         System.out.println(persona1.presentarse());
+        persona1.Edad();
+
         System.out.println("La edad de " + persona1.getNombre() + " en días es: " + persona1.edadEnDias());
         System.out.println("¿Es mayor de edad? " + (persona1.esMayorDeEdad() ? "Sí" : "No"));
-        
+
         System.out.println();
-        
+
         System.out.println(persona2.presentarse());
+        persona2.Edad();
         System.out.println("La edad de " + persona2.getNombre() + " en días es: " + persona2.edadEnDias());
-        System.out.println("¿Es mayor de 18 años? " + (persona2.esMayorDeEdad(18) ? "Sí" : "No"));
+        System.out.println("¿Es mayor de 18 años? " + (persona2.esMayorDeEdad() ? "Sí" : "No"));
     }
 }
--- Clase03_POO/src/clase03_poo/Persona.java
+++ Clase03_POO/src/clase03_poo/Persona.java
@@ -1,27 +1,15 @@
-/*
-     * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-     * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package clase03_poo;
 
-/**
- *
- * @author javie
- */
 public class Persona {
 
     String nombre;
     int edad;
 
-    public void Nombre() {
-        System.out.println("Cristian");
+    public String presentarse() {
+        return "Hola, mi nombre es " + nombre + " y tengo " + edad + " años.";
     }
 
-    public void Apellidos() {
-        System.out.println("Cueva");
-    }
-
-    public void Edad(int edad) {
+    public void Edad() {
         System.out.println("Tengo " + edad + " años");
     }
 
@@ -39,10 +27,6 @@ public class Persona {
 
     public String getNombre() {
         return nombre;
-    }
-
-    public String presentarse() {
-        return "Hola, mi nombre es " + nombre + " y tengo " + edad + " años.";
     }
 
     public boolean esMayorDeEdad() {
